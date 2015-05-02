510d4a89ba53f2b2b40cbb3966cd8a4a11bcc6d1
Johannes Lochmann <appstackgen@gmail.com>
2015-05-02 20:40:01 +0200

    added Owns edge

---
+++ src/lib/asg/graph/owns.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Project 'appstackgen'.
+ *
+ * Copyright (c) 2015, Johannes Lochmann
+ *
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <asg/graph/systemedge.h>
+
+namespace ASG {
+namespace Graph {
+
+class Owns : public SystemEdge
+{
+public:
+  using SystemEdge::SystemEdge;
+};
+
+}
+}
