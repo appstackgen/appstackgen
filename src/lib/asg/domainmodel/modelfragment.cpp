008f37242501e421d9a3e674d95a5433ce000d34
Johannes Lochmann <appstackgen@gmail.com>
2015-05-06 16:34:04 +0200

    ModelFragment

---
+++ src/lib/asg/domainmodel/modelfragment.cpp
@@ -0,0 +1,15 @@
+/*
+ * This file is part of Project 'appstackgen'.
+ *
+ * Copyright (c) 2015, Johannes Lochmann
+ *
+ * All rights reserved.
+ */
+
+#include "modelfragment.h"
+
+namespace ASG {
+namespace DomainModel {
+
+}
+}
