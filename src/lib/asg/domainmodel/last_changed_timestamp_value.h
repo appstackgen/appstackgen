f6c696a7224b0c53fddeaa254d21f7c6d90b21aa
Johannes Lochmann <appstackgen@gmail.com>
2015-05-16 20:46:37 +0200

    derived from concurrency_control_value

--- src/lib/asg/domainmodel/last_changed_timestamp_value.h
+++ src/lib/asg/domainmodel/last_changed_timestamp_value.h
@@ -8,18 +8,18 @@
 
 #pragma once
 
-#include <asg/domainmodel/persistence_value.h>
+#include <asg/domainmodel/concurrency_control_value.h>
 
 namespace asg {
 namespace domain_model {
 
-class last_changed_timestamp_value: public persistence_value
+class last_changed_timestamp_value: public concurrency_control_value
 {
 public:
     static string static_node_type_name;
     static string default_name;
 
-    using persistence_value::persistence_value;
+    using concurrency_control_value::concurrency_control_value;
 
 protected:
     string impl_node_type_name() const override { return static_node_type_name; }
