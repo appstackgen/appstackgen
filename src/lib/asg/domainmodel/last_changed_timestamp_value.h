bf0e0a717d022f5eb12e920e7e0eb2de91413240
Johannes Lochmann <appstackgen@gmail.com>
2015-05-16 20:49:45 +0200

    derived from audit_value

--- src/lib/asg/domainmodel/last_changed_timestamp_value.h
+++ src/lib/asg/domainmodel/last_changed_timestamp_value.h
@@ -8,18 +8,18 @@
 
 #pragma once
 
-#include <asg/domainmodel/concurrency_control_value.h>
+#include <asg/domainmodel/audit_value.h>
 
 namespace asg {
 namespace domain_model {
 
-class last_changed_timestamp_value: public concurrency_control_value
+class last_changed_timestamp_value: public audit_value
 {
 public:
     static string static_node_type_name;
     static string default_name;
 
-    using concurrency_control_value::concurrency_control_value;
+    using audit_value::audit_value;
 
 protected:
     string impl_node_type_name() const override { return static_node_type_name; }
