7f34f491a39c23ce0d37db06def73c7ea9892b8f
Johannes Lochmann <appstackgen@gmail.com>
2015-05-16 20:45:24 +0200

    introduced concurrency_control_value

---
+++ src/lib/asg/domainmodel/concurrency_control_value.cpp
@@ -0,0 +1,15 @@
+/*
+ * This file is part of Project 'appstackgen'.
+ *
+ * Copyright (c) 2015, Johannes Lochmann
+ *
+ * All rights reserved.
+ */
+
+#include "concurrency_control_value.h"
+
+namespace asg {
+namespace domain_model {
+
+}
+}
---
+++ src/lib/asg/domainmodel/concurrency_control_value.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Project 'appstackgen'.
+ *
+ * Copyright (c) 2015, Johannes Lochmann
+ *
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <asg/domainmodel/persistence_value.h>
+
+namespace asg {
+namespace domain_model {
+
+class concurrency_control_value : public persistence_value
+{
+public:
+    using persistence_value::persistence_value;
+};
+
+}
+}
